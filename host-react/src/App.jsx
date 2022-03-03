6aaa6897ea9167864422cd7e07cb36e60383eacf
Michael Varrieur <michael.varrieur@boldpenguin.com>
2022-03-03 13:21:24 -0500

    Use search component in host

--- host-react/src/App.jsx
+++ host-react/src/App.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import Search from 'remote_react/Search';
 
 import "./index.scss";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <h1>Host React App</h1>
-  </div>
-);
+const App = () => {
+  const [pokemon, setPokemon] = useState({});
+
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <h1>Host React App</h1>
+      <Search onSuccess={(data) => setPokemon(data)} />
+    </div>
+  )
+};
 ReactDOM.render(<App />, document.getElementById("app"));
